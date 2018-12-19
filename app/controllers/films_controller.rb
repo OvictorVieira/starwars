5232ac945a31173b20f39cc6005f075823bbb19f
OvictorVieira <viih@live.fr>
2018-12-19 20:30:18 -0200

    Ajusta o retorno da api caso ocorra erro

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,20 +2,24 @@ class FilmsController < ApplicationController
   before_action :film_by_api, only: [:index]
 
   def index
-    @films = film_by_api
-  end
-
-  private
-
-  def film_by_api
     begin
       response = call_swapi('?format=json')
 
       raise unless response.success?
 
-      response['results']
+      @films = response['results']
     rescue
       flash[:warning] = I18n.t(:'messages.error.call_api_error')
     end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def film_by_api
+
   end
 end
